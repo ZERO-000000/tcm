7264e018aefc3ffd7cdc1399908a3e1f5fa52d16
smalldeng <hacker_xinlang@sina.com>
2019-12-06 08:16:54 +0800

    11

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router'
 import axios from 'axios'
 import qs from 'qs'
 import VueAxios from 'vue-axios'
-import ElementUI from 'element-ui'
+import ElementUI, {Message} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(VueAxios, axios);
@@ -29,19 +29,38 @@ axios.interceptors.request.use(function(config){
 //添加一个响应拦截器
 axios.interceptors.response.use(function(res){
   //在这里对返回的数据进行处理
+  console.log('数据返回');
   return res;
 },function(err){
   //Do something with response error
-  return Promise.reject(error);
+  switch (err.response.status) {
+    case 504:
+      Message.error("服务器异常，请联系管理员！");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("passWord");
+      localStorage.removeItem("token");
+      router.push("/login");
+    case 999:
+      Message.error("会话已超时，请重新登录系统！");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("passWord");
+      localStorage.removeItem("token");
+      router.push("/login");
+  }
+  return Promise.reject(err);
 })
 //如果未登录，跳转到登录页面；如果已经登录，确保要调用next()方法，否则钩子就不会被resolved
 router.beforeEach((to, from, next)=>{
   /* 判断该路由是否需要登录权限 */
   if (to.matched.some(record => record.meta.requireAuth)){
-    //console.log('需要登录');
+    console.log('路由之前判断');
     if (localStorage.token){
       // 判断当前的token是否存在 ； 登录存入的token
-      //console.log("localStorage.token"+localStorage.token);
+      console.log("localStorage.token"+localStorage.token);
+      let params={
+        token:localStorage.token
+      };
+      axios.post('/checkLoginSession',qs.stringify(params));
       next();
     }else{
       next({
